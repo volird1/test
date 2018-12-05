25860a19de6fbb88d22ce8c2a23a345f5d796c41
pythonowiec <45623593+pythonowiec@users.noreply.github.com>
2018-12-05 10:39:08 +0100

    Create gra.py

---
+++ gra.py
@@ -0,0 +1,15 @@
+a = "plemię wody"
+b = "plemię ziemi"
+c = "plemię ognia"
+
+plemiona = input("podaj wybrane plemię a-plemię wody b-plemię ziemi c-plemię ognia")
+if plemiona == "a":
+ print("od teraz jesteś wojownikiem plemiona wody, twoje tereny leżą wzdłuż morza [nazwa morza]. Twoje plemię żyje z rybołustwa, jedną z zalet twojego plemiona jest posiadanie floty handlowej i wojennej umiesz świetnie walczyć na morzu  i radzisz sobie na lądzie. Twoje zalety to odwaga ")
+
+
+if plemiona == "b":
+ print ("od teraz jestes wojownikiem ziemi, twoje tereny to ziemia[nazwa ziemi]. Twoją przewaga jest teren wszystkie lasy, łaki itd. jesteś szybszy od innych plemion masz dobre rozpoznawanie terenu i dobrze panujesz nad lądem")
+
+if plemiona == "c":
+
+ print ("Twoją nailepszą pozycją jest ogien posiadasz bardzo dużę umiejetności. Posiadasz przewage nad wszystkimi przeciwnikami w ogniu czujesz sie jak ryba w wodzie.Twoje główne zalety to ognista zbroja i ognisty miecz.")

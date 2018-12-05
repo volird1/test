760b1d1d28bb4ceedc2e4df57333ec5f103c1f92
pythonowiec <45623593+pythonowiec@users.noreply.github.com>
2018-12-05 11:30:21 +0100

    Update gra.py

--- gra.py
+++ gra.py
@@ -1,15 +1,35 @@
-a = "plemię wody"
+a = "plemię nadwodne"
 b = "plemię ziemi"
-c = "plemię ognia"
+c = "plemię górskie"
 
-plemiona = input("podaj wybrane plemię a-plemię wody b-plemię ziemi c-plemię ognia")
-if plemiona == "a":
- print("od teraz jesteś wojownikiem plemiona wody, twoje tereny leżą wzdłuż morza [nazwa morza]. Twoje plemię żyje z rybołustwa, jedną z zalet twojego plemiona jest posiadanie floty handlowej i wojennej umiesz świetnie walczyć na morzu  i radzisz sobie na lądzie. Twoje zalety to odwaga ")
+print("wpisz literę plemiona od a do c, aby zapoznać sie z opisem plemion")
+wybor = input()
+
+if wybor == "a":
+ print('''Od teraz jesteś wojownikiem plemiona wody, twoje tereny leżą wzdłuż morza [nazwa morza]. 
+ Twoje plemię żyje z rybołóstwa. 
+ Jedną z zalet twojego plemiona jest posiadanie floty handlowej i wojennej. 
+ Umiesz świetnie walczyć na morzu i radzisz sobie na lądzie.''')
+ wybor = input()
 
 
-if plemiona == "b":
- print ("od teraz jestes wojownikiem ziemi, twoje tereny to ziemia[nazwa ziemi]. Twoją przewaga jest teren wszystkie lasy, łaki itd. jesteś szybszy od innych plemion masz dobre rozpoznawanie terenu i dobrze panujesz nad lądem")
+if wybor == "b":
+ 
+  print ('''Od teraz jestes wojownikiem ziemi, twoje tereny to ziemia[nazwa ziemi]. Twoim terenem są wszystkie lasy i  
+  łaki. Utrzymujesz się z rolnictwa. W twój dorobek wchodzą rózne kopalnie i huty i warsztaty.''')
+  wybor = input()
 
-if plemiona == "c":
 
- print ("Twoją nailepszą pozycją jest ogien posiadasz bardzo dużę umiejetności. Posiadasz przewage nad wszystkimi przeciwnikami w ogniu czujesz sie jak ryba w wodzie.Twoje główne zalety to ognista zbroja i ognisty miecz.")
+if wybor == "c":
+
+  print ('''Twoje tereny są w górach [nazwa gór]. Twoimi zaletami jest budowanie warowni w trudno dostępnych miejscach wysoko w górach. Twoi wojownicy są wytrzymali na duży wysiłek fizyczny ''')
+  
+  
+
+plemiona = input("podaj wybrane plemię a-plemię nadwodne b-plemię ziemi c-plemię górskie")
+if plemiona == 'a':
+  print("plemie nadwodne" )
+if plemiona == 'b':
+  print("plemie ziemi" )
+if plemiona == 'c':
+  print("plemie górskie" 
